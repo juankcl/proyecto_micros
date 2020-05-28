e7168978ed7b6630b096b85e75cf0a0f8c00f578
Emilio Puga <ea.pugaavalos@ugto.mx>
2020-05-27 20:08:48 -0500

    LCD mostrar dia de la semana

--- Source/lcd.asm
+++ Source/lcd.asm
@@ -1,16 +1,17 @@
 ; Pantalla LCD PIC
 LIST P = 18f45K50
 #include<p18f45K50.inc>
-
+#include<common.inc>
+	
 ; Funciones externas
 EXTERN Retardo
-
+AuxLcd EQU 0X02
+	
 CODE
 
-; Inicializar la pantalla lcd
 LCDInit:
 	Call Retardo
-	MOVLW h'0F' ; Comando para prender la pantalla con cursor
+	MOVLW h'0C' ; Comando para prender la pantalla
 	CALL LCD_Comm
 
 	MOVLW h'38' ; Comando para configurar la pantalla en modo 8 bits
@@ -39,4 +40,180 @@ LCD_Char:
 	Call Retardo
 	RETURN
 	GLOBAL LCD_Char
+
+; Mostrar dias de la semana
+SABADO:
+    MOVLW 'S'
+	CALL LCD_Char
+	MOVLW 'a'
+	CALL LCD_Char
+	MOVLW 'b'
+	CALL LCD_Char
+	MOVLW 'a'
+	CALL LCD_Char
+	MOVLW 'd'
+	CALL LCD_Char
+	MOVLW 'o'
+	CALL LCD_Char
+	MOVLW ' '
+	CALL LCD_Char
+	MOVLW ' '
+	CALL LCD_Char
+	MOVLW ' '
+	CALL LCD_Char
+    RETURN
+DOMINGO:
+    MOVLW 'D'
+	CALL LCD_Char
+	MOVLW 'o'
+	CALL LCD_Char
+	MOVLW 'm'
+	CALL LCD_Char
+	MOVLW 'i'
+	CALL LCD_Char
+	MOVLW 'n'
+	CALL LCD_Char
+	MOVLW 'g'
+	CALL LCD_Char
+	MOVLW 'o'
+	CALL LCD_Char
+	MOVLW ' '
+	CALL LCD_Char
+	MOVLW ' '
+	CALL LCD_Char
+    RETURN
+
+; Mostrar el dia de la semana en la pantalla
+LCDDia:
+	
+	MOVWF AuxLcd
+	MOVLW h'80'
+	CALL LCD_Comm
+	
+	MOVF AuxLcd, w
+	
+	XORLW 0
+	BZ LUNES
+		
+	XORLW 1^0
+	BZ MARTES
+		
+	XORLW 2^1
+	BZ MIERCOLES
+	
+	XORLW 3^2
+	BZ JUEVES
+	
+	XORLW 4^3
+	BZ VIERNES
+	
+	XORLW 5^4
+	BZ SABADO
+	
+	XORLW 6^5
+	BZ DOMINGO
+	
+	RETURN
+	GLOBAL LCDDia
+
+LUNES:
+    MOVLW 'L'
+	CALL LCD_Char
+	MOVLW 'u'
+	CALL LCD_Char
+	MOVLW 'n'
+	CALL LCD_Char
+	MOVLW 'e'
+	CALL LCD_Char
+	MOVLW 's'
+	CALL LCD_Char
+	MOVLW ' '
+	CALL LCD_Char
+	MOVLW ' '
+	CALL LCD_Char
+	MOVLW ' '
+	CALL LCD_Char
+	MOVLW ' '
+	CALL LCD_Char
+    RETURN
+MARTES:
+    MOVLW 'M'
+	CALL LCD_Char
+	MOVLW 'a'
+	CALL LCD_Char
+	MOVLW 'r'
+	CALL LCD_Char
+	MOVLW 't'
+	CALL LCD_Char
+	MOVLW 'e'
+	CALL LCD_Char
+	MOVLW 's'
+	CALL LCD_Char
+	MOVLW ' '
+	CALL LCD_Char
+	MOVLW ' '
+	CALL LCD_Char
+	MOVLW ' '
+	CALL LCD_Char
+    RETURN
+MIERCOLES:
+    MOVLW 'M'
+	CALL LCD_Char
+	MOVLW 'i'
+	CALL LCD_Char
+	MOVLW 'e'
+	CALL LCD_Char
+	MOVLW 'r'
+	CALL LCD_Char
+	MOVLW 'c'
+	CALL LCD_Char
+	MOVLW 'o'
+	CALL LCD_Char
+	MOVLW 'l'
+	CALL LCD_Char
+	MOVLW 'e'
+	CALL LCD_Char
+	MOVLW 's'
+	CALL LCD_Char
+    RETURN
+JUEVES:
+    MOVLW 'J'
+	CALL LCD_Char
+	MOVLW 'u'
+	CALL LCD_Char
+	MOVLW 'e'
+	CALL LCD_Char
+	MOVLW 'v'
+	CALL LCD_Char
+	MOVLW 'e'
+	CALL LCD_Char
+	MOVLW 's'
+	CALL LCD_Char
+	MOVLW ' '
+	CALL LCD_Char
+	MOVLW ' '
+	CALL LCD_Char
+	MOVLW ' '
+	CALL LCD_Char
+    RETURN
+VIERNES:
+    MOVLW 'V'
+	CALL LCD_Char
+	MOVLW 'i'
+	CALL LCD_Char
+	MOVLW 'e'
+	CALL LCD_Char
+	MOVLW 'r'
+	CALL LCD_Char
+	MOVLW 'n'
+	CALL LCD_Char
+	MOVLW 'e'
+	CALL LCD_Char
+	MOVLW 's'
+	CALL LCD_Char
+	MOVLW ' '
+	CALL LCD_Char
+	MOVLW ' '
+	CALL LCD_Char
+    RETURN
 end
--- Source/main.asm
+++ Source/main.asm
@@ -1,10 +1,11 @@
 ; Programa que genera interrupciones
 LIST P = 18f45K50
 #include<p18f45K50.inc> 
-
+#include<common.inc>
+		
 ; Funciones externas
 ; LCD
-EXTERN LCDInit, LCD_Comm, LCD_Char
+EXTERN LCDInit, LCD_Comm, LCD_Char, LCDDia
 	
 ; Seccion de bits de configuracion inicial
 CONFIG WDTEN = OFF  ; Disables the Watchdog
@@ -15,14 +16,10 @@ CONFIG FOSC = INTOSCIO ; Enables  the internal oscillator
   
 ; Declaracion de variables en la memoria de datos
 Aux1 EQU 0x00 ; Reserves register 0
-Aux2 EQU 0x01  ; Reserves register 1
-Base1 EQU 0x02
-Segundero EQU 0x03
 
 ; Directiva que indica en que direccion de la memoria de programa se colocara la primera instruccion    
 org 0
 GOTO Start 
-
 
 Start:
 	MOVLB 0x0F
@@ -39,12 +36,13 @@ Start:
 	MOVWF OSCCON
 
 	; Inicializar la pantalla LCD
-	Call Retardo
+	CALL Retardo
 	CALL LCDInit
+
+	; Mandar un numero del 0 al 6, 0 siendo lunes y 6 siendo domingo
 	
-	; Mostrar la letra 'H'
-	MOVLW 'H'
-	CALL LCD_Char
+	MOVLW 3
+	CALL LCDDia
 	
 ;Ciclo principal
 MainLoop:
